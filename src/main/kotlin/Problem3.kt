f05f074aa2eff17c0850b41ff26b689b79bd0555
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-18 15:18:10 +0300

    Added solution for problem3

---
+++ src/main/kotlin/Problem3.kt
@@ -0,0 +1,9 @@
+import org.w3c.dom.ranges.Range
+
+fun main(){
+    println(containsRange(5..8, 5..9))
+}
+
+fun containsRange(range1: ClosedRange<Int>, range2: ClosedRange<Int>) : Boolean{
+    return range1.start < range2.start && range1.endInclusive > range2.endInclusive
+}
